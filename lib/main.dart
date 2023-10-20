9e42e705f8634ed47c635247fd26a11b44fdafb3
pranavtaysheti <pranavtaysheti@gmail.com>
2023-10-20 17:32:41 +0530

    upgrade gradle and flutter embedding

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,47 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(XylophoneApp());
+void main() => runApp(const MyApp());
 
-class XylophoneApp extends StatelessWidget {
+void playSound(int name) async {
+  final player = AudioPlayer();
+  await player.play(AssetSource("note$name.wav"));
+}
+
+Widget buildKey({required Color color, required int sound}) {
+  return Expanded(
+    child: TextButton(
+      onPressed: () {
+        playSound(sound);
+      },
+      child: Container(
+        color: color,
+      ),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(color: Colors.red, sound: 1),
+              buildKey(color: Colors.orange, sound: 2),
+              buildKey(color: Colors.yellow, sound: 3),
+              buildKey(color: Colors.green, sound: 4),
+              buildKey(color: Colors.teal, sound: 5),
+              buildKey(color: Colors.blue, sound: 6),
+              buildKey(color: Colors.purple, sound: 7),
+            ],
+          ),
         ),
       ),
     );
